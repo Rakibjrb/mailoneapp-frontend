5ea1566e39aa9512b0a11c76c83ae448bbec31c1
Rakibjrb <rakibjrb572157@gmail.com>
2026-01-18 21:25:27 +0600

    completed footer section

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
 import "../globals.css";
 
 export default function MainLayout({ children }: { children: ReactNode }) {
@@ -7,6 +8,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1 w-full">{children}</main>
+      <Footer />
     </div>
   );
 }
